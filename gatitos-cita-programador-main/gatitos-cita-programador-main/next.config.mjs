2c6c43b751e26ab6f00977432b3900c95c135e98
Javilf00 <159240784+Javilf00@users.noreply.github.com>
2026-09-20 12:27:21 +0200

    Configure Next.js for GitHub Pages

--- gatitos-cita-programador-main/gatitos-cita-programador-main/next.config.mjs
+++ gatitos-cita-programador-main/gatitos-cita-programador-main/next.config.mjs
@@ -1,5 +1,14 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/preparasion-sita" : "",
+  assetPrefix: isProd ? "/preparasion-sita/" : "",
 };
 
 export default nextConfig;
